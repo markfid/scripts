9c275ef1b20359211857e229b5268a62eb59ad80
Mark Fidell <git@fids.org.uk>
2016-08-18 15:45:36 +0100

    Added check that the user is root

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$(whoami)" != "root" ]
+then
+    echo This script must be run as root.
+    exit
+fi
+
 apt-get update
 apt-get upgrade
 apt-get --assume-yes install vim
