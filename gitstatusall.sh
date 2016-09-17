ef7954d73d0d42d0282c2e2416885a8e37dfae3b
Mark Fidell <git@fids.org.uk>
2016-09-17 12:54:20 +0100

    Made it so Symlinks to directories are treated as directories themselves

--- gitstatusall.sh
+++ gitstatusall.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s)' \;
+find -L . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && git status -s)' \;
