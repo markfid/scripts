430e9bdda1ca15e1a99e519f376e599e5e967670
Mark <markfid@users.noreply.github.com>
2017-04-27 12:01:53 +0100

    Added haveged

--- ubuntu-vm-setup.sh
+++ ubuntu-vm-setup.sh
@@ -4,5 +4,6 @@ sudo apt-get install vim
 sudo apt-get install git
 sudo apt-get install linux-source
 sudo apt-get install open-vm-tools-desktop
+sudo apt-get install haveged
 #sudo apt-get install open-vm-tools
 
